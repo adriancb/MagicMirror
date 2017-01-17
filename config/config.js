2431cda40bae97d8c60afbffb833a4495bb0509f
Adrian CB <abarbul@gmail.com>
2017-01-17 16:06:30 +1100

    Adding config with AUified content.

---
+++ config/config.js
@@ -0,0 +1,82 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+var config = {
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
+
+	language: 'en',
+	timeFormat: 24,
+	units: 'metric',
+
+	modules: [
+		{
+			module: 'alert',
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: 'clock',
+			position: 'top_left'
+		},
+		{
+			module: 'calendar',
+			header: 'AU Holidays',
+			position: 'top_left',
+			config: {
+				calendars: [
+					{
+						symbol: 'calendar-check-o ',
+						url: 'webcal://www.calendarlabs.com/templates/ical/Australia-Holidays.ics'
+					}
+				]
+			}
+		},
+		{
+			module: 'compliments',
+			position: 'lower_third'
+		},
+		{
+			module: 'currentweather',
+			position: 'top_right',
+			config: {
+				location: 'Melbourne',
+				locationID: '2158177',  //ID from http://www.openweathermap.org
+				appid: 'acb5faaedb2471807ecbddb05afeb691'
+			}
+		},
+		{
+			module: 'weatherforecast',
+			position: 'top_right',
+			header: 'Weather Forecast',
+			config: {
+				location: 'Melbourne',
+				locationID: '2158177',  //ID from http://www.openweathermap.org
+				appid: 'acb5faaedb2471807ecbddb05afeb691'
+			}
+		},
+		{
+			module: 'newsfeed',
+			position: 'bottom_bar',
+			config: {
+				feeds: [
+					{
+						title: "ABC News",
+						url: "http://www.abc.net.au/news/feed/51120/rss.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true
+			}
+		},
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== 'undefined') {module.exports = config;}
